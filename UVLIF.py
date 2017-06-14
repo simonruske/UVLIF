5f818a4f939493812b906ea32335e65ab6bb490f
simon.ruske@postgrad.manchester.ac.uk <simon.ruske@postgrad.manchester.ac.uk>
2017-06-14 07:50:37 +0100

    Added cleaning functionality

--- UVLIF.py
+++ UVLIF.py
@@ -1,6 +1,13 @@
-import os
+import os, sys
 from UVLIF2.configuration.load_config import load_config_files
 from UVLIF2.read.read import read_files
+from UVLIF2.utils.cleaner import clean
 
 cfg = load_config_files()
-read_files(cfg)
+if len(sys.argv) == 1:
+  read_files(cfg)
+
+elif sys.argv[1] == 'clean':
+  clean(cfg, sys.argv[2:])
+
+
