5a66ab969af783080fd75c3f4bf7974c3930e774
simon.ruske@postgrad.manchester.ac.uk <simon.ruske@postgrad.manchester.ac.uk>
2017-07-27 10:22:33 +0100

    Added functionality to create filelist from UVLIF.py

--- UVLIF.py
+++ UVLIF.py
@@ -3,6 +3,7 @@ from UVLIF2.configuration.load_config import load_config_files
 from UVLIF2.read.read import read_files
 from UVLIF2.plot.plot import plot
 from UVLIF2.utils.cleaner import clean
+from UVLIF2.utils.filelist import create_filelist_laboratory
 
 cfg = load_config_files()
 if len(sys.argv) == 1:
@@ -11,5 +12,14 @@ if len(sys.argv) == 1:
 
 elif sys.argv[1] == 'clean':
   clean(cfg, sys.argv[2:])
+
+elif sys.argv[1] == 'create' and sys.argv[2] == 'filelist':
+  input_directory = os.path.join(cfg['main_directory'], "data")
+  output_directory = os.path.join(cfg['main_directory'], "output")
+  create_filelist_laboratory(cfg, input_directory, output_directory)
+
+else:
+  print("Did not recognise that command")
+
 
 
