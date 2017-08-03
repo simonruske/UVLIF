a1d122a3f5ac4d6bd47b1b07b03e047e9e84cb56
simon.ruske@postgrad.manchester.ac.uk <simon.ruske@postgrad.manchester.ac.uk>
2017-08-03 14:40:14 +0100

    added cleaner function for results

--- UVLIF2/utils/cleaner.py
+++ UVLIF2/utils/cleaner.py
@@ -16,13 +16,19 @@ def clean(cfg, filenames):
     the output directory
   '''
 
+  directory = os.path.join(cfg['main_directory'], 'output')
+
   if filenames[0] == 'all':
     filenames = os.listdir(os.path.join(cfg['main_directory'], 'output'))
 
   elif filenames[0] == 'data':
     filenames = ['data.csv', 'labels.csv', 'FT.csv', 'startend.csv', 'times.csv']
 
+  elif filenames[0] == 'results':
+    filenames = ['results.csv']
+    directory = os.path.join(directory, "results")
+
   for filename in filenames:
-    full_filename = os.path.join(cfg['main_directory'], 'output', filename)
+    full_filename = os.path.join(directory, filename)
     if os.path.exists(full_filename):
       os.remove(full_filename)
