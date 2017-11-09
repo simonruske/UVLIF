60ed4cd1cda7c4db5b54b12d976f8ff2aeb25957
simon <simon.ruske@postgrad.manchester.ac.uk>
2017-11-09 10:38:56 +0000

    Added argument for gui

--- UVLIF.py
+++ UVLIF.py
@@ -6,22 +6,29 @@ from UVLIF2.utils.cleaner import clean
 from UVLIF2.utils.files import check
 from UVLIF2.utils.filelist import create_filelist_laboratory
 from UVLIF2.analysis.analysis import analyse
+from UVLIF2.gui.gui import run
 
-cfg = load_config_files()
+
 if len(sys.argv) == 1:
+  cfg = load_config_files()
   read_files(cfg)
   analyse(cfg)
   plot(cfg)
 
 elif sys.argv[1] == 'clean':
+  cfg = load_config_files()
   check(cfg)
   clean(cfg, sys.argv[2:])
 
 elif sys.argv[1] == 'create' and sys.argv[2] == 'filelist':
+  cfg = load_config_files()
   check(cfg)
   input_directory = os.path.join(cfg['main_directory'], "data")
   output_directory = os.path.join(cfg['main_directory'], "output")
   create_filelist_laboratory(cfg, input_directory, output_directory)
+
+elif sys.argv[1] == 'gui':
+  run()
 
 else:
   print("Did not recognise that command")
