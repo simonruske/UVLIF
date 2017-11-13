53b011c3249f12c18f5b2de170f4465ea2032929
simon <simon.ruske@postgrad.manchester.ac.uk>
2017-11-13 13:25:44 +0000

    Added analysis configuration window to the interface update script

--- ui.sh
+++ ui.sh
@@ -2,3 +2,4 @@ pyuic5 UVLIF2/gui/main.ui -o UVLIF2/gui/main_ui.py
 pyuic5 UVLIF2/gui/analysis.ui -o UVLIF2/gui/analysis_ui.py
 pyuic5 UVLIF2/gui/configuration.ui -o UVLIF2/gui/configuration_ui.py
 pyuic5 UVLIF2/gui/filelist.ui -o UVLIF2/gui/filelist_ui.py
+pyuic5 UVLIF2/gui/analysis_configuration.ui -o UVLIF2/gui/analysis_configuration_ui.py
