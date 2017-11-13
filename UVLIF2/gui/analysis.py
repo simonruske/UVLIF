c21687aecdede13c623440307ed90b5f7e865b60
simon <simon.ruske@postgrad.manchester.ac.uk>
2017-11-13 13:31:41 +0000

    Added connecting functions for the analysis configuration window

--- UVLIF2/gui/analysis.py
+++ UVLIF2/gui/analysis.py
@@ -1,4 +1,5 @@
 import UVLIF2.gui.analysis_ui as main
+from UVLIF2.gui.analysis_configuration import analysis_configuration_window
 from PyQt5 import QtWidgets, QtCore
 import os
 from UVLIF2.configuration.load_config import load_config
@@ -9,12 +10,35 @@ class analysis_window(QtWidgets.QDialog, main.Ui_Dialog):
   def __init__(self, parent = None):
     super(analysis_window, self).__init__(parent)
     self.setupUi(self)
+    self.cfg = {}
+    self.analysis_options = {}
     self.shorthand = {}
     self.populate_list()
+
+    self.analysis_configuration_window = analysis_configuration_window(self)
+    self.msgBox = QtWidgets.QMessageBox()
+   
+
+    # connecting buttons
+    self.editButton.clicked.connect(self.edit)
     self.checkAllButton.clicked.connect(self.check_all)
     self.uncheckAllButton.clicked.connect(self.uncheck_all)
     self.saveButton.clicked.connect(self.save)
-    self.cfg = {}
+ 
+
+
+
+  def edit(self):
+    
+    item = self.listWidget.currentItem()
+    if item == None:
+      self.msgBox.setText("Please select a method to be edited")
+      self.msgBox.exec_()
+    else:
+      self.analysis_configuration_window.setWindowTitle("Editing " + item.text())
+      self.analysis_configuration_window.method = item.text()
+      self.analysis_configuration_window.update()
+      self.analysis_configuration_window.show()
     
 
   def populate_list(self):
@@ -23,11 +47,14 @@ class analysis_window(QtWidgets.QDialog, main.Ui_Dialog):
     analysis_list_directory = os.path.abspath(analysis_list_directory) #convert .. to directory
     for item in os.listdir(analysis_list_directory):
       cfg = load_config(os.path.join(analysis_list_directory, item))
+      
       item = str(item).replace('.proto', '')
       
       self.listWidget.addItem(item)
       
       self.shorthand[item] = cfg['shorthand']
+      self.analysis_options.update(cfg)
+      del self.analysis_options['shorthand']
       
 
     #add checkbox to each
